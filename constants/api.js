ca8a7cc8ea08520e7096290afececcca031d97d8
Vinicius <vinicius.ferreira258@etec.sp.gov.br>
2025-08-11 23:56:26 -0300

    consumir api no apk - bacnkend hospedado

--- constants/api.js
+++ constants/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({ 
-    baseURL: 'http://192.168.2.100:3000',
+    baseURL: 'https://backend-test-54i9.onrender.com',
     timeout: 10000,
 });
 
